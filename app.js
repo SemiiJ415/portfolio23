a6cede07c1cbd07ee62682273eab43e085d69693
semiij415 <jarissa415@gmail.com>
2023-10-24 15:24:44 -0500

    updates

--- app.js
+++ app.js
@@ -1,3 +1,17 @@
 // window.addEventListener('scroll', () => {
 //     document.body.style.setProperty('--scroll', window.pageYOffset / (document.body.offsetHeight - window.innerHeight));
 // }, false);
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
+
+
+
+//  .top-line { 
+width: 51%;
+position: absolute;
+right: 24px; 
+left: 40px;
+top: 20px;
+border: 1px solid #146a7b;
+}
