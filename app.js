9807b013f10ff08cbc3164866d7d82abb3dd9a02
SemiiJ415 <Jarissa415@gmail.com>
2023-10-31 12:02:23 -0500

    mobile updates

--- app.js
+++ app.js
@@ -7,11 +7,20 @@ const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstra
 
 
 
-//  .top-line { 
-width: 51%;
-position: absolute;
-right: 24px; 
-left: 40px;
-top: 20px;
-border: 1px solid #146a7b;
-}
+const themeBtn = document.getElementById('themeBtn');
+
+themeBtn.addEventListener('click', ()=> {
+    // console.log('you clicked me!');
+
+    const html = document.getElementsByTagName('html')[0];
+
+    // console.log(html);
+
+    html.getAttribute('data-bs-theme') == 'light' ? html.setAttribute('data-bs-theme','dark') : html.setAttribute('data-bs-theme','light')
+});
+
+// bootstrap popover
+
+const popoverTriggerList = document.querySelectorAll('[data-bs-toggle="popover"]')
+
+const popoverList = [...popoverTriggerList].map(popoverTriggerEl => new bootstrap.Popover(popoverTriggerEl))
